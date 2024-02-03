f9cce5cf781bca71b5241af04753c0430c34dff5
akshat <akshatbhatya@gmail.com>
2024-02-03 14:40:42 +0530

    add validaton

--- manage account details/src/Components/Login.jsx
+++ manage account details/src/Components/Login.jsx
@@ -1,39 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Validation from './Validation';
 
 function Login() {
+
+    let initialData = {
+        fullname: "",
+        email: "",
+        number: "",
+        password: ""
+
+    }
+    let [value, setValue] = useState(initialData);
+    let [error, setError] = useState(null)
+
+
+
+    let changeEvent = (e) => {
+        let target = e.target;
+        setValue((prev) => ({ ...prev, [target.name]: target.value }))
+        console.log(value);
+
+    }
+    let handleForm = (e) => {
+        e.preventDefault()
+        let message = Validation(value.email, value.password, value.number);
+        setError(message)
+    }
     return (
         <div className='flex justify-center items-center mt-40'>
 
-            <form action="" className='bg-[#f0f0f0] inline-block p-4 rounded-md shadow-md'>
+            <form action="" className='bg-[#f0f0f0] inline-block p-4 rounded-md shadow-md' onSubmit={handleForm}>
 
                 <label htmlFor="" className='capitalize'>Full Name
                     <br />
-                    <input type="text" placeholder='full name' className='capitalize bg-slate-200 rounded-md px-2' />
+                    <input type="text" placeholder='Full Name' className=' bg-slate-200 rounded-md px-2' value={value.fullname} name="fullname" onChange={changeEvent} />
                 </label>
 
                 <br />
 
                 <label htmlFor="" className='capitalize'>email
                     <br />
-                    <input type="text" placeholder='email' className='capitalize bg-slate-200 rounded-md px-2' />
+                    <input type="text" placeholder='Email' className=' bg-slate-200 rounded-md px-2' value={value.email} name="email" onChange={changeEvent} />
                 </label>
 
                 <br />
 
                 <label htmlFor="" className='capitalize'>number
                     <br />
-                    <input type="text" placeholder='9876543210' className='capitalize bg-slate-200 rounded-md px-2' />
+                    <input type="text" placeholder='9876543210' className=' bg-slate-200 rounded-md px-2' value={value.number} name="number" onChange={changeEvent} />
                 </label>
                 <br />
 
                 <label htmlFor="" className='capitalize'>password
                     <br />
-                    <input type="text" placeholder='password' className='capitalize bg-slate-200 rounded-md px-2' />
+                    <input type="text" placeholder='Password' className=' bg-slate-200 rounded-md px-2' value={value.password} name="password" onChange={changeEvent} />
                 </label>
 
                 <br />
+                <span className='text-red-600'>{error}</span>
                 <br />
-                <button className='bg-green-700 text-white rounded-sm px-3'>Submit</button>
+                <button className='bg-green-700 text-white rounded-sm px-3' type='Submit'>Submit</button>
             </form>
 
         </div>
--- manage account details/src/Components/Validation.jsx
+++ manage account details/src/Components/Validation.jsx
@@ -1,12 +1,14 @@
-function Validation(email, password, number) {
+function Validation(Email, Password, Number) {
 
-    let email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
-    let password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
-    let number = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(number)
+    let emailVerification = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(Email)
+    let passwordVerification = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(Password)
+    let numberVerification = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(Number)
 
-    if (email) return "not valid email"
-    if (password) return "not valid password"
-    if (number) return "not valid password"
+    console.log(emailVerification);
+
+    if (!emailVerification) return "not valid email"
+    if (!passwordVerification) return "not valid password"
+    if (!numberVerification) return "not valid password"
 
     return null
 }
