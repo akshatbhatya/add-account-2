79343a6ab010de5e9daf28fe3540099093c13a7c
akshat <akshatbhatya@gmail.com>
2024-02-03 14:02:24 +0530

    add login form

--- manage account details/src/App.jsx
+++ manage account details/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Components/Login'
 
 function App() {
   
   return(
     <>
-    <h2 className='text-red-600'>app</h2>
+   <Login/>
     </>
   )
 }
--- manage account details/src/Components/Login.jsx
+++ manage account details/src/Components/Login.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>
+    return (
+        <div className='flex justify-center items-center mt-40'>
 
-        <form action="">
+            <form action="" className='bg-[#f0f0f0] inline-block p-4 rounded-md shadow-md'>
 
-            <label htmlFor="">Full Name 
-            <input type="text" placeholder='full name' className='capitalize'/>
-            </label>
-        </form>
-      
-    </div>
-  )
+                <label htmlFor="" className='capitalize'>Full Name
+                    <br />
+                    <input type="text" placeholder='full name' className='capitalize bg-slate-200 rounded-md px-2' />
+                </label>
+
+                <br />
+
+                <label htmlFor="" className='capitalize'>email
+                    <br />
+                    <input type="text" placeholder='email' className='capitalize bg-slate-200 rounded-md px-2' />
+                </label>
+
+                <br />
+
+                <label htmlFor="" className='capitalize'>number
+                    <br />
+                    <input type="text" placeholder='9876543210' className='capitalize bg-slate-200 rounded-md px-2' />
+                </label>
+                <br />
+
+                <label htmlFor="" className='capitalize'>password
+                    <br />
+                    <input type="text" placeholder='password' className='capitalize bg-slate-200 rounded-md px-2' />
+                </label>
+
+                <br />
+                <br />
+                <button className='bg-green-700 text-white rounded-sm px-3'>Submit</button>
+            </form>
+
+        </div>
+    )
 }
 
 export default Login
