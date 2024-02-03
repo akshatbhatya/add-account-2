51e7764132f8c51d98a0906083ed0cf9231cc383
akshat <akshatbhatya@gmail.com>
2024-02-03 13:49:23 +0530

    add tailwind css

---
+++ manage account details/src/Components/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+
+        <form action="">
+
+            <label htmlFor="">Full Name 
+            <input type="text" placeholder='full name' className='capitalize'/>
+            </label>
+        </form>
+      
+    </div>
+  )
+}
+
+export default Login
